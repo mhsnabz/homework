2f77ea445c79e0f0bb932fd5fae4e82c22202330
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-18 17:14:10 +0300

    no msg

---
+++ E-commerce/E-commerce/Model/CurrentUser.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentUser.swift
+//  E-commerce
+//
+//  Created by mahsun abuzeyitoğlu on 18.07.2020.
+//  Copyright © 2020 mahsun abuzeyitoğlu. All rights reserved.
+//
+
+import Foundation
