14567f145ccda1057455dc9f20173e8827ab7327
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-30 14:25:54 +0300

    no msg

--- E-commerce/E-commerce/Controller/KID/KidVC.swift
+++ E-commerce/E-commerce/Controller/KID/KidVC.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+private let manCell = "manCell"
 
 class KidVC: UIViewController {
-    
+    var collectionview: UICollectionView!
+    var currentUser : CurrentUser!
     let titleLbl : UILabel = {
        let lbl = UILabel()
         lbl.text = "ÇOCUK"
@@ -41,9 +43,19 @@ class KidVC: UIViewController {
         super.viewDidLoad()
 
      configureUI()
+         configureCollectionView()
     }
     
-
+    func configureCollectionView(){
+           let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+           collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+           collectionview.dataSource = self
+           collectionview.delegate = self
+           collectionview.backgroundColor = .white
+           collectionview.register(ManVcCell.self, forCellWithReuseIdentifier: manCell)
+           view.addSubview(collectionview)
+           collectionview.anchor(top: headerBar.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, rigth: view.rightAnchor, marginTop: 0, marginLeft: 0, marginBottom: 0, marginRigth: 0, width: 0, heigth: 0)
+       }
     func configureUI(){
         view.backgroundColor = .white
         view.addSubview(headerBar)
@@ -54,4 +66,65 @@ class KidVC: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+extension KidVC : UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: manCell, for: indexPath) as! ManVcCell
+        let menuOption = kidVcOption(rawValue: indexPath.row)
+        cell.img.image = menuOption?.image
+        cell.name.text = menuOption?.description
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.view.frame.size.width - 10)/2, height: (self.view.frame.size.height)/3)
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = ProductVC()
+        let menuOption = kidVcOption(rawValue: indexPath.row)
+   
+        if menuOption?.description == "Ayakkabı"{
+            vc.titleText = "Çocuk Ayakkabı"
+            vc.typeModel = "shoes"
+            vc.type = "shoes"
+            
+        }else  if menuOption?.description == "Pantolon"{
+            vc.titleText = "Çocuk Pantolon"
+            vc.typeModel = "pantolon"
+            vc.type = "pantolon"
+        }
+        else  if menuOption?.description == "T-Shirt"{
+            vc.titleText = "Çocuk T-Shirt"
+            vc.typeModel = "tshirt"
+            vc.type = "tshirt"
+        }
+        else  if menuOption?.description == "Eşofmanlar"{
+            vc.titleText = "Çocuk Eşofman"
+            vc.typeModel = "gym"
+            vc.type = "gym"
+        }else  if menuOption?.description == "Şortlar"{
+            vc.titleText = "Çocuk Şortlar"
+            vc.typeModel = "sortlar"
+            vc.type = "sortlar"
+        }
+        else  if menuOption?.description == "Formalar"{
+            vc.titleText = "Çocuk Formalar"
+            vc.typeModel = "formalar"
+            vc.type = "formalar"
+        }else{
+            vc.titleText = "Çocuk Çeket"
+            vc.typeModel = "ceketler"
+            vc.type = "ceketler"
+        }
+        vc.gender = "kid"
+        vc.currentUser = currentUser
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    
 }
--- E-commerce/E-commerce/Helper/MenuOptionManVC.swift
+++ E-commerce/E-commerce/Helper/MenuOptionManVC.swift
@@ -91,3 +91,45 @@ enum womanVcOption : Int ,CustomStringConvertible {
         }
     }
 }
+enum kidVcOption : Int ,CustomStringConvertible {
+    case ayakkabi
+    case pantolon
+    case ceketler
+    case tshirt
+    case gym
+    case sortlar
+    case formalar
+    
+    
+    
+    var description: String{
+        switch self {
+        case .ayakkabi:
+            return "Ayakkabı"
+        case .pantolon :
+            return "Pantolon"
+        case.tshirt:
+            return "T-Shirt"
+        case .gym:
+            return "Eşofmanlar"
+        case .sortlar:
+            return "Şortlar"
+        case .formalar:
+            return "Formalar"
+        case .ceketler:
+            return "Çeketler"
+            
+        }
+    }
+    var image : UIImage {
+        switch self {
+        case .ayakkabi : return UIImage(named: "kid-shoes")!
+        case .pantolon : return UIImage(named: "kid-jean")!
+        case.tshirt : return UIImage(named: "kid-tshirt")!
+        case .ceketler : return UIImage(named: "kid-jacket")!
+        case .formalar: return UIImage(named: "kid-forma")!
+        case .gym: return UIImage(named: "kid-gym")!
+        case .sortlar : return UIImage(named :"kid-sort")!
+        }
+    }
+}
