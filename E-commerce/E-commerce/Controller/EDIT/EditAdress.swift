1b2a6b9be4c60b6327c2c445682fa9190be18efd
mhsnabz <mhsnabz@yahoo.com.tr>
2020-08-06 03:41:32 +0300

    no msg

--- E-commerce/E-commerce/Controller/EDIT/EditAdress.swift
+++ E-commerce/E-commerce/Controller/EDIT/EditAdress.swift
@@ -9,11 +9,11 @@
 import UIKit
 import FirebaseFirestore
 class EditAdress: UIViewController {
-
+    
     var currentUser : CurrentUser!
     
     let titleLbl : UILabel = {
-       let lbl = UILabel()
+        let lbl = UILabel()
         lbl.text = "Adresinizi Güncelleyin"
         lbl.font = UIFont(name: Utilities.font, size: 15)
         lbl.textColor = .white
@@ -26,7 +26,7 @@ class EditAdress: UIViewController {
         return btn
     }()
     lazy var headerBar : UIView = {
-       let v = UIView()
+        let v = UIView()
         v.backgroundColor = .menuColor()
         v.addSubview(dissmisButton)
         dissmisButton.anchor(top: nil, left: v.leftAnchor, bottom: nil, rigth: nil, marginTop: 0, marginLeft: 20, marginBottom: 0, marginRigth: 0, width: 20, heigth: 20)
@@ -34,7 +34,7 @@ class EditAdress: UIViewController {
         v.addSubview(titleLbl)
         titleLbl.anchor(top: nil, left: nil, bottom: nil, rigth: nil, marginTop: 0, marginLeft: 0, marginBottom: 0, marginRigth: 0, width: 0, heigth: 0)
         titleLbl.centerYAnchor.constraint(equalTo: v.centerYAnchor).isActive = true
-                titleLbl.centerXAnchor.constraint(equalTo: v.centerXAnchor).isActive = true
+        titleLbl.centerXAnchor.constraint(equalTo: v.centerXAnchor).isActive = true
         
         return v
     }()
@@ -45,32 +45,32 @@ class EditAdress: UIViewController {
         return lbl
     }()
     let lblCity : UILabel = {
-           let lbl = UILabel ()
-           lbl.font = UIFont(name: Utilities.fontBold, size: 14)
-           lbl.text = "İlçe / İl"
-           return lbl
-       }()
+        let lbl = UILabel ()
+        lbl.font = UIFont(name: Utilities.fontBold, size: 14)
+        lbl.text = "İlçe / İl"
+        return lbl
+    }()
     let address : UITextView = {
-          let txt = UITextView()
+        let txt = UITextView()
         
-           txt.layer.borderWidth = 0.3
-           txt.dataDetectorTypes = .all
-           txt.layer.borderColor = UIColor.lightGray.cgColor
-           txt.isSelectable = true
-           txt.text = String(txt.text.prefix(140))
-           return txt
-       }()
+        txt.layer.borderWidth = 0.3
+        txt.dataDetectorTypes = .all
+        txt.layer.borderColor = UIColor.lightGray.cgColor
+        txt.isSelectable = true
+        txt.text = String(txt.text.prefix(140))
+        return txt
+    }()
     let city : UITextView = {
-            let txt = UITextView()
-          
-             txt.layer.borderWidth = 0.3
-             txt.dataDetectorTypes = .all
-             txt.layer.borderColor = UIColor.lightGray.cgColor
-             txt.isSelectable = true
-             txt.text = String(txt.text.prefix(140))
-             return txt
-         }()
-   
+        let txt = UITextView()
+        
+        txt.layer.borderWidth = 0.3
+        txt.dataDetectorTypes = .all
+        txt.layer.borderColor = UIColor.lightGray.cgColor
+        txt.isSelectable = true
+        txt.text = String(txt.text.prefix(140))
+        return txt
+    }()
+    
     let save : UIButton = {
         let btn = UIButton(type: .system)
         btn.setTitle("Kaydet", for: .normal)
@@ -104,22 +104,22 @@ class EditAdress: UIViewController {
         save.anchor(top: city.bottomAnchor, left: view.leftAnchor, bottom: nil, rigth: view.rightAnchor, marginTop: 40, marginLeft: 70, marginBottom: 40, marginRigth: 70, width: 0, heigth: 50)
         Utilities.styleFilledButton(save)
         save.addTarget(self, action: #selector(saveFunc), for: .touchUpInside)
-       }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-    
         
-     
+        
+        
     }
     
-   
-
+    
+    
     //MARK: -handlers
     @objc func dissmis(){
-          self.dismiss(animated: true, completion: nil)
-      }
+        self.dismiss(animated: true, completion: nil)
+    }
     @objc func saveFunc()
     {
         let db = Firestore.firestore().collection("user")
@@ -145,34 +145,34 @@ extension EditAdress: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         let size = CGSize(width: view.frame.width, height: 300)
         let estimatedSize = textView.sizeThatFits(size)
-
-       if textView.contentSize.height >= 300
-       {
-        textView.isScrollEnabled = true
-       }
-       else
-           {
-           textView.frame.size.height = textView.contentSize.height
+        
+        if textView.contentSize.height >= 300
+        {
+            textView.isScrollEnabled = true
+        }
+        else
+        {
+            textView.frame.size.height = textView.contentSize.height
             textView.isScrollEnabled = false // textView.isScrollEnabled = false for swift 4.0
-       }
+        }
         textView.constraints.forEach { (constraint) in
             if constraint.firstAttribute == .height {
-
+                
                 if textView.contentSize.height >= 300
                 {
-                 textView.isScrollEnabled = true
-                     constraint.constant = 300
+                    textView.isScrollEnabled = true
+                    constraint.constant = 300
                 }
                 else
-                    {
+                {
                     textView.frame.size.height = textView.contentSize.height
-                     textView.isScrollEnabled = false // textView.isScrollEnabled = false for swift 4.0
-                        constraint.constant = estimatedSize.height
+                    textView.isScrollEnabled = false // textView.isScrollEnabled = false for swift 4.0
+                    constraint.constant = estimatedSize.height
                 }
-              //  constraint.constant = estimatedSize.height
+                //  constraint.constant = estimatedSize.height
             }
         }
     }
-   
+    
     
 }
