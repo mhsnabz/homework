a888f2b547d7b91aaccf60204e65eb91c886504f
mhsnabz <mhsnabz@yahoo.com.tr>
2020-08-07 00:13:09 +0300

    no msg

--- E-commerce/E-commerce/Home/HomeVC.swift
+++ E-commerce/E-commerce/Home/HomeVC.swift
@@ -15,8 +15,8 @@ class HomeVC: UIViewController {
     
     var currentUser : CurrentUser!{
         didSet{
-
-
+            
+            
             getCartItems()
         }
     }
@@ -28,11 +28,11 @@ class HomeVC: UIViewController {
     
     
     var collectionview: UICollectionView!
-      var carAnimaiton = AnimationView()
+    var carAnimaiton = AnimationView()
     
-  
+    
     let badgeBtn : UIButton = {
-       let btn = UIButton()
+        let btn = UIButton()
         btn.clipsToBounds = true
         btn.layer.cornerRadius = 10
         btn.setBackgroundColor(color: .red, forState: .normal)
@@ -41,7 +41,7 @@ class HomeVC: UIViewController {
         btn.titleLabel?.font = UIFont(name: Utilities.fontBold, size: 10)
         return btn
     }()
-     var anim = AnimationView()
+    var anim = AnimationView()
     lazy var cart : UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -51,8 +51,8 @@ class HomeVC: UIViewController {
         view.layer.shadowOffset = CGSize (width: 0, height: 5)
         view.layer.shadowRadius = 8
         view.layer.shadowOpacity = 0.3
-       let tap = UITapGestureRecognizer(target: self, action: #selector(handleTab))
-       view.addGestureRecognizer(tap)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTab))
+        view.addGestureRecognizer(tap)
         anim = .init(name: "car")
         anim.animationSpeed = 1
         anim.loopMode = .loop
@@ -74,13 +74,13 @@ class HomeVC: UIViewController {
         btn.layer.shadowOffset = CGSize (width: 25, height: 25)
         btn.layer.shadowRadius = 25
         btn.layer.shadowOpacity = 0.6
-    
+        
         return btn
     }()
     
     
     override func viewWillAppear(_ animated: Bool) {
-
+        
         carAnimaiton.play()
         anim.play()
         if currentUser == nil {
@@ -97,9 +97,9 @@ class HomeVC: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
         self.view.backgroundColor = .white
-       setNavigationBar()
+        setNavigationBar()
         
         configureCollectionView()
         
@@ -111,7 +111,7 @@ class HomeVC: UIViewController {
         badgeBtn.anchor(top: cart.topAnchor, left: nil, bottom: nil, rigth: cart.rightAnchor, marginTop: -8, marginLeft: 0, marginBottom: 0, marginRigth: 8, width: 20, heigth: 20)
         
     }
-
+    
     
     func getCartItems(){
         let db = Firestore.firestore().collection("user")
@@ -133,60 +133,60 @@ class HomeVC: UIViewController {
     }
     //MARK: -functions
     @objc func handleTab(){
-
+        
         let vc = CartVC()
         vc.currentUser = currentUser
         
         self.present(vc, animated: true, completion: nil)
-            
+        
     }
     func configureCollectionView(){
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-              collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-              collectionview.dataSource = self
-              collectionview.delegate = self
+        collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionview.dataSource = self
+        collectionview.delegate = self
         collectionview.backgroundColor = .white
         collectionview.register(HomeVcCell.self, forCellWithReuseIdentifier: cellId)
         view.addSubview(collectionview)
         collectionview.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, rigth: view.rightAnchor, marginTop: 0, marginLeft: 0, marginBottom: 0, marginRigth: 0, width: 0, heigth: 0)
     }
-        func setNavigationBar()  {
-      
-            
-            carAnimaiton = .init(name: "car")
-                   carAnimaiton.animationSpeed = 1
-                   carAnimaiton.loopMode = .loop
-            carAnimaiton.play()
-                         NSLayoutConstraint.activate([
-                             carAnimaiton.heightAnchor.constraint(equalToConstant: 50),
-                             carAnimaiton.widthAnchor.constraint(equalToConstant: 50)
-                         ])
-               
-               navigationItem.titleView = carAnimaiton
-           
-            let menuBtn = UIButton(type: .custom)
-              menuBtn.frame = CGRect(x: 0.0, y: 0.0, width: 20, height: 20)
-                       menuBtn.setImage(UIImage(named:"menu"), for: .normal)
-                       menuBtn.addTarget(self, action: #selector(menuClick), for: .touchUpInside)
-                  let menuBarItem = UIBarButtonItem(customView: menuBtn)
-                  let currWidth = menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 24)
-                  currWidth?.isActive = true
-                  let currHeight = menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 24)
-                  currHeight?.isActive = true
-                  self.navigationItem.leftBarButtonItem = menuBarItem
+    func setNavigationBar()  {
+        
+        
+        carAnimaiton = .init(name: "car")
+        carAnimaiton.animationSpeed = 1
+        carAnimaiton.loopMode = .loop
+        carAnimaiton.play()
+        NSLayoutConstraint.activate([
+            carAnimaiton.heightAnchor.constraint(equalToConstant: 50),
+            carAnimaiton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        navigationItem.titleView = carAnimaiton
+        
+        let menuBtn = UIButton(type: .custom)
+        menuBtn.frame = CGRect(x: 0.0, y: 0.0, width: 20, height: 20)
+        menuBtn.setImage(UIImage(named:"menu"), for: .normal)
+        menuBtn.addTarget(self, action: #selector(menuClick), for: .touchUpInside)
+        let menuBarItem = UIBarButtonItem(customView: menuBtn)
+        let currWidth = menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 24)
+        currWidth?.isActive = true
+        let currHeight = menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 24)
+        currHeight?.isActive = true
+        self.navigationItem.leftBarButtonItem = menuBarItem
     }
-
+    
     @objc func menuClick(){
         self.delegate?.handleMenuToggle(forMenuOption: nil)
         if !isMenuOpen {
             self.isMenuOpen = false
-                         
-           
+            
+            
         }else{
-           
-                self.isMenuOpen = true
-          
-          
+            
+            self.isMenuOpen = true
+            
+            
         }
         
     }
@@ -199,8 +199,8 @@ extension HomeVC : UICollectionViewDataSource, UICollectionViewDelegateFlowLayou
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeVcCell
         cell.backgroundColor = .white
-//
-     
+        //
+        
         cell.img.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 130)
         let menuOption = CollectionViewOption(rawValue: indexPath.row)
         cell.img.image = menuOption?.image
@@ -211,7 +211,7 @@ extension HomeVC : UICollectionViewDataSource, UICollectionViewDelegateFlowLayou
         return CGSize(width: self.view.frame.width , height: 130 )
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-       return UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        return UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
     }
     
 }
